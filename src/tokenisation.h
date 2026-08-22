1932ca82594663d9323d0cca4343048e19415b81
Sky Lee <skylee01244@gmail.com>
2026-08-23 02:41:50 +1000

    Implemented tokeniser for exit,int,semi

--- src/tokenisation.h
+++ src/tokenisation.h
@@ -1,2 +1,76 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+enum class TokenType {
+    exit,
+    int_lit,
+    semi
+};
+
+struct Token {
+    TokenType type;
+    std::optional<std::string> value {};
+};
+
+class Tokeniser {
+public:
+    inline explicit Tokeniser(std::string src): m_src(std::move(src)) {}
+
+    inline std::vector<Token> tokenise() {
+        std::vector<Token> tokens;
+        std::string buf {};
+        while(peek().has_value()) {
+            if(std::isalpha(peek().value())) {
+                buf.push_back(consume());
+                while(peek().has_value() && std::isalnum(peek().value())) {
+                    buf.push_back(consume());
+                }
+                if(buf == "exit") {
+                    tokens.push_back({.type = TokenType::exit});
+                    buf.clear();
+                    continue;
+                } else {
+                    std::cerr << "You messed up" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else if(std::isdigit(peek().value())) {
+                buf.push_back(consume());
+                while(peek().has_value() && std::isdigit(peek().value())) {
+                    buf.push_back(consume());
+                }
+                tokens.push_back({.type = TokenType ::int_lit, .value = buf});
+                buf.clear();
+                continue;
+            }
+            else if(peek().value() == ';') {
+                consume();
+                tokens.push_back({.type = TokenType::semi});
+                continue;
+            }
+            else if(std::isspace(peek().value())) {
+                consume();
+                continue;
+            } else {
+                std::cerr << "You messed up" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        m_index = 0;
+        return tokens;
+    }
+private:
+    [[nodiscard]] inline std::optional<char> peek(int ahead = 0) const {
+        if(m_index + ahead >= m_src.length()) return {};
+        else    return m_src.at(m_index + ahead); // Added + ahead here
+    }
+
+    inline char consume() {
+        return m_src.at(m_index++);
+    }
+
+    const std::string m_src;
+    size_t m_index = 0;
+};
