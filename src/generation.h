6a6ac14fa73084822d586a2a454d5973e4c253e8
Sky Lee <skylee01244@gmail.com>
2026-08-23 02:39:58 +1000

    Added Code generation class

---
+++ src/generation.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "./parser.h"
+
+class Generator {
+public:
+    inline Generator(NodeExit root) :
+        m_root(std::move(root)) {}
+
+    [[nodiscard]] std::string generate() const
+    {
+        std::stringstream output;
+        output << "global _main\n";
+        output << "section .text\n";
+        output << "_main:\n";
+        output << "    mov rax, 0x2000001\n";
+        output << "    mov rdi, " << m_root.expr.int_lit.value.value() << "\n";
+        output << "    syscall";
+        return output.str();
+    }
+
+private:
+    const NodeExit m_root;
+
+
+};
